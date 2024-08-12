134deb5953ad4fee443e248b757779fa70ee1f6a
revadisundaram7 <sundaramrevadi@gmail.com>
2024-08-12 14:34:31 +0530

    form

--- portfolio-app/src/components/Contact.js
+++ portfolio-app/src/components/Contact.js
@@ -2,14 +2,14 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div name="contact" className='font-Raleway w-full h-screen bg-gradient-to-b from-gray-800 via-black to-black text-white px-4 py-10'>
+    <div name="contact" className='font-Raleway w-full h-screen bg-gradient-to-b from-gray-800 via-black to-black text-white px-4 py-10' >
         <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
           <div className='pb-8'>
                 <p className='text-4xl font-bold inline border-b-4 border-gray-500'>Contact </p>
                 <p className='py-6'>Submit the form below to get in touvh with me.</p>
             </div>
             <div className='flex justify-center items-center'>
-              <form action='https://getform.io/f/azylrorb' method='POST' className='flex flex-col w-full md:w-1/2'>
+              <form action='https://getform.io/f/azylrorb' method='POST' className='flex flex-col w-full md:w-1/2' netlify>
                 <input type='text' name='email' placeholder='Enter your name'
                   className='p-2 bg-transparent border-2 rounded-md text-white focus:outline-none' />
                   <input type='email' name='name' placeholder='Enter your email'
